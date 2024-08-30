e681afd03142b3f41cc6a646c2919d44f5ca5e88
redssy <redlemagic@hotmail.com>
2024-08-30 15:13:47 -0300

    primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue$': 'vue/dist/__globalTypes_3.4_false.d.ts'
+      'vue$': 'vue/dist/vue.esm-bundler.js'
     }
   }
 })
