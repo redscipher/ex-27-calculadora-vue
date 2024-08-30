cca308b3d3ddec3510fb2287613c75c3912c8f63
redssy <redlemagic@hotmail.com>
2024-08-30 16:12:15 -0300

    chore: alterando titulo

--- src/componentes/Cabecalho.vue
+++ src/componentes/Cabecalho.vue
@@ -3,7 +3,7 @@
     <header class="p-5 m-5 bg-dark rounded-3 text-center text-white">
         <div class="row">
             <div class="col-12">
-                <h1>Calculadora</h1>
+                <h1>Calculadora infinita</h1>
             </div>
         </div>
     </header>
