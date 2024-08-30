fdf7b5ecd3cd7cb2ba0e308149263b8c1001c2e9
redssy <redlemagic@hotmail.com>
2024-08-30 12:28:24 -0300

    primeiro commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle'
 // css
 import './sass/index.scss';
 // ts
