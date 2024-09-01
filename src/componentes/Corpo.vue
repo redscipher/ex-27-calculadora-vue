734098cdc292460758ad7523cf7e22af3433358b
redssy <redlemagic@hotmail.com>
2024-09-01 13:34:01 -0300

    feat: subindo app

--- src/componentes/Corpo.vue
+++ src/componentes/Corpo.vue
@@ -1,62 +1,30 @@
 <script setup lang="ts">
-// variaveis
-let flgNumerico = true;
-
-//constantees
-const numerosCalculadora: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'C', '0', 'E'];
-const operacoes: string[] = ['+', '-', '*', '/'];
-
-// funcoes
-function validaNumerico(num: string): boolean{
-    // var retorno
-    let controle: boolean = false;
-    try {
-        // valida se eh um numero
-        if (parseInt(num) >= 0) controle= true;
-    } catch (error) {
-        console.log(error);
-    } finally {
-        return controle;
-    }
-}
+// propriedade
+const props = defineProps(['num1', 'num2', 'resultado', 'recuperaCampos', 'recuperaCampo2', 'trocaOperacao', 'operacao']);
 // 
 </script>
 
 <template>
     <!--  -->
-    <div class="container pt-5 text-center text-white">
+    <div class="container pt-5 pe-5 text-center text-white">
         <div class="row">
             <div class="col-12">
                 <form action="" class="ps-4 py-5 d-flex">
                     <div class="w-75 d-flex">
-                        <input class="form-control mb-3 me-3" type="number" placeholder="Digite um número">
-                        <select name="" id="" class="form-select mb-3 me-3 w-25 rounded-3">
+                        <input id="id-num1" v-model="props.num1" @change="props.recuperaCampos"
+                            class="form-control mb-3 me-3" type="number" placeholder="Digite um número">
+                        <select v-model="props.operacao" @change="props.trocaOperacao" name="" id="" class="form-select mb-3 me-3 w-25 rounded-3">
                             <option value="1" selected>+</option>
                             <option value="2">-</option>
                             <option value="3">*</option>
                             <option value="4">/</option>
                         </select>
-                        <input class="form-control mb-3" type="number" placeholder="Digite um número">
+                        <input id="id-num2" v-model="props.num2" @change="props.recuperaCampo2"
+                            class="form-control mb-3" type="number" placeholder="Digite um número">
                     </div>
-                    <label id="id-resultado" for="" class="form-label w-25 ms-3 mb-3 text-center">Resultado</label>
+                    <label id="id-resultado" for="" class="form-label w-25 ms-3 mb-3 text-center">{{ props.resultado
+                        }}</label>
                 </form>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-10">
-                <ul class="row">
-                    <li class="col-4 py-3" v-for="num in numerosCalculadora">
-                        <button v-if="validaNumerico(num)" type="button" class="btn btn-secondary w-100">{{ num }}</button>
-                        <button v-else type="button" class="btn btn-warning w-100">{{ num }}</button>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-2">
-                <ul class="row">
-                    <li class="col-12 py-3" v-for="op in operacoes">
-                        <button type="button" class="btn btn-light  w-100">{{ op }}</button>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
@@ -64,20 +32,18 @@ function validaNumerico(num: string): boolean{
 </template>
 
 <style scoped lang="scss">
-    // importacoes
-    @use '../sass/variaveis';
+// importacoes
+@use '../sass/variaveis';
 
-    // elementos
-    ul {
-        list-style: none;
-    }
+// elementos
+ul {
+    list-style: none;
+}
 
-    // ids
-    #id-resultado{
-        border: 2px dotted #ccc;
-        border-radius: 10px;
-        background-color: lighten(variaveis.$corFundo, 30%);
-    }
-
-    // 
+// ids
+#id-resultado {
+    border: 2px dotted #ccc;
+    border-radius: 10px;
+    background-color: lighten(variaveis.$corFundo, 30%);
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 // icones bootstrap
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 // css
 import './sass/index.scss';
-// ts
-import './ts/index';
 
 // cria instancia objeto
 createApp(App).mount('#app')
